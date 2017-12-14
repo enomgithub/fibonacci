d8b2dbe28d53f2b491b71dfc742139ba81d54742
enom <satoshi.enom@gmail.com>
2017-12-14 20:23:52 +0900

    Modify: Add 'when isMainModule:'

--- fibonacci.nim
+++ fibonacci.nim
@@ -2,7 +2,7 @@
 from strutils import parseInt
 
 
-proc fibonacci(n: int): int =
+proc fibonacci*(n: int): int =
   # nが0未満の場合、例外[AssertionError]を送出する
   assert(n >= 0)
   if n == 0:
@@ -13,6 +13,9 @@ proc fibonacci(n: int): int =
     return fibonacci(n - 2) + fibonacci(n - 1)
 
 
-# 標準入力からの入力が10進数の整数でない場合は、例外[ValueError]を送出する
-let n: int = parseInt(readLine(stdin))
-echo "n = ", n, ", fibonacci number: f(n) = ", fibonacci(n)
+# メインモジュールならば以下を実行する
+# Python の if __name__ = '__main__': に相当
+when isMainModule:
+  # 標準入力からの入力が10進数の整数でない場合は、例外[ValueError]を送出する
+  let n: int = parseInt(readLine(stdin))
+  echo "n = ", n, ", fibonacci number: f(n) = ", fibonacci(n)
